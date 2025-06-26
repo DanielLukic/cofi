90b4bc64d16f8b791a0271ec41813e81f53f342f
Daniel Lukic <daniel.lukic@gmx.net>
2025-06-26 08:37:58 +0200

    Extract CLI args and GTK window positioning to separate files

---
+++ src/gtk_window.h
@@ -0,0 +1,12 @@
+#ifndef GTK_WINDOW_H
+#define GTK_WINDOW_H
+
+#include <gtk/gtk.h>
+#include "app_data.h"
+
+// Window positioning and alignment functions
+void validate_saved_position(AppData *app, GdkScreen *screen);
+void apply_window_position(GtkWidget *window, WindowAlignment alignment);
+void on_window_size_allocate(GtkWidget *window, GtkAllocation *allocation, gpointer user_data);
+
+#endif // GTK_WINDOW_H
