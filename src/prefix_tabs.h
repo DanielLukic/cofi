b1a01d84afedf30fbe3072c3f3dc16bfdba7fbcd
Daniel Lukic <daniel.lukic@gmx.net>
2026-05-07 06:10:29 +0200

    feat: prefix→tab autoswitch with origin restore

---
+++ src/prefix_tabs.h
@@ -0,0 +1,9 @@
+#ifndef PREFIX_TABS_H
+#define PREFIX_TABS_H
+
+#include "app_data.h"
+
+void clear_prefix_tab_claim(AppData *app);
+void apply_prefix_tab_claim(AppData *app, const char *entry_text);
+
+#endif
